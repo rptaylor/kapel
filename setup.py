5198e0b5c02e0cde9c6922aaab2988181699bdbf
Ryan Taylor <1686627+rptaylor@users.noreply.github.com>
2021-05-12 17:47:36 -0700

    try setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+from os import environ
+
+environ['PIP_NO_WARN_SCRIPT_LOCATION'] = "0"
+
+setup(name='kapel',
+#      version='0.1',
+      packages = find_packages(),
+      )
