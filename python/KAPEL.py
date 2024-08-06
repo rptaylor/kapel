31bea1cfe3fa623053fc92dbf014ce08dedc7d94
Matthew Westphall <westphall@wisc.edu>
2024-08-06 16:29:05 -0500

    Assert that records being generated for Gratia have a nonzero processor count

--- python/KAPEL.py
+++ python/KAPEL.py
@@ -271,6 +271,9 @@ def record_individual_period(config, results):
         # Only report on pods that have completed. Running pods won't have an endtime
         if not ('starttime' in records and 'endtime' in records):
             continue
+        processors = records.get('cores', 0) or config.processors
+        assert processors > 0, \
+            "must specify pilot CPU count via pod resource requests, or PROCESSORS config var"
         individual_output = individual_message(
             config, 
             pod_name,
